206643e6fb41f018df23fc8a720c0998d0907846
Lubasha <parakhonya1@yandex.ru>
2024-01-02 20:03:57 +0300

    after review

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -6,27 +6,24 @@ function solveEquation(a, b, c) {
       let x = (-b + Math.sqrt(dis))/(2*a);
       let y = (-b - Math.sqrt(dis))/(2*a);
       arr.push(x, y);
-      return arr
-    } else if (dis === 0) {
+    } if (dis === 0) {
     let x = -b/(2*a);
     arr.push(x);
-      return arr
-  } else { 
-      return arr
-    }
+  } 
+  return arr
 }
 
 
 
             
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-let P = ((percent /100) / 12);
-let S = amount - contribution;
-let n = (1 + P)**countMonths;
-let payment = S * (P + (P / (n - 1)));
-let N = (payment * 12).toFixed(2);
+let rate = ((percent /100) / 12);
+let loanBody = amount - contribution;
+let raisedDegree = (1 + rate)**countMonths;
+let payment = loanBody * (rate + (rate / (raisedDegree - 1)));
+let paymentRound = (payment * countMonths).toFixed(2);
 
-  return +N
+  return +paymentRound
 }
 
 
