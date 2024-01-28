5a210352c124bace13a57588383823c117bc2f78
Lubasha <parakhonya1@yandex.ru>
2024-01-28 21:13:17 +0300

    exaption-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount (num){
+    testNum = Number.parseFloat(num)
+    if(typeof num === "number" ){
+      return console.log(num)
+    }
+    if(isNaN(testNum) === true){
+      throw new Error("Невалидное значение")
+      } 
+    return console.log(Number.parseFloat(num))
+  }
+  
+  
+  
+  function validateCount(num) {
+    try{
+      return parseCount(num)
+    }
+    catch(arr){
+      return console.log(arr)
+    }
+  }
+
+  class Triangle{
+    constructor(a, b, c){
+      this.a = a
+      this.b = b
+      this.c = c
+      if(this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a){
+        throw new Error("Треугольник с такими сторонами не существует")
+      }
+    }
+  
+    get perimeter(){
+      return this.a + this.b + this.c
+    }
+  
+    get area(){
+      palfPerimeter = this.perimeter()/2
+      areaTriangle = +Math.sqrt(palfPerimeter * (palfPerimeter - this.a) * (palfPerimeter - this.b) * (palfPerimeter - this.c)).toFixed(2)
+      return areaTriangle
+    }
+    
+  }
+  
+  
+  function getTriangle(a, b, c){
+    try{
+      return  new Triangle(a, b, c)
+    }
+    catch(arr){
+      console.log(arr)
+    }
+  
+  }
